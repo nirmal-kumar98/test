9b4a11e4be90c3207777745b59c2c4c6ebdab2dc
NirmalKumars_98 <61598808+nirmal-kumar98@users.noreply.github.com>
2023-02-27 22:04:43 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  backendURL: 'https://master.d32amxg3pn4gws.amplifyapp.com/'
+  backendURL: 'https://master.d32amxg3pn4gws.amplifyapp.com'
   // backendURL: 'http://localhost:5000'
 };
